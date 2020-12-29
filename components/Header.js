721aaa0f50e181f16943c1c4c6d8c2dce90d0b51
Vanessa Dixon <vanessazdixon@gmail.com>
2020-12-29 16:06:51 -0500

    completed header.js

--- components/Header.js
+++ components/Header.js
@@ -10,5 +10,23 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+let headerContainer = document.querySelector('.header-container');
+function Header() {
+    const div = document.createElement('div');
+    div.classList.add('header');
 
-function Header() {}
+    const span = document.createElement('span');
+    span.textContent = 'March 28, 2020';
+    span.classList.add('date');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Lambda Times';
+
+    const span2 = document.createElement('span');
+    span2.classList.add('temp');
+
+    div.append(span,h1,span2);
+    return div;
+}
+
+headerContainer.append(Header());
