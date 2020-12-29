6b914bc59b1afe3376e69189e7e181caf67b21b0
Vanessa Dixon <vanessazdixon@gmail.com>
2020-12-29 18:00:29 -0500

    completed mvp

--- components/Cards.js
+++ components/Cards.js
@@ -1,22 +1,43 @@
-// STEP 3: Create article cards.
-// -----------------------
-// Send an HTTP GET request to the following address: https://lambda-times-api.herokuapp.com/articles
-// Study the response data you get back, closely.
-// You will be creating a card for each article in the response.
-// This won't be as easy as just iterating over an array though.
-//
-// Write a function that takes a single article object and returns the following markup:
-//
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {author's name}</span>
-//   </div>
-// </div>
-//
-// Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-//
-// Use your function to create a card for each of the articles, and append each card to the DOM.
+    const cardsContainer = document.querySelector('.cards-container');
+
+
+    function articleCard(articleObj) {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        const div2 = document.createElement('div');
+        div2.classList.add('headline');
+        div2.textContent = `${articleObj.headline}`;
+        const div3 = document.createElement('div');
+        div3.classList.add('author');
+
+        const div4 = document.createElement('div');
+        div4.classList.add('img-container');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', `${articleObj.authorPhoto}`);
+
+        const span = document.createElement('span');
+        span.textContent = `By ${articleObj.authorName}`;
+
+        div.append(div2,div3,div4,span);
+        div3.append(div4);
+        div4.append(img);
+
+        div.addEventListener('click', function() {
+            console.log(`${articleObj.headline}`)
+        })
+        return div;
+    }
+
+    const arr = ["javascript", "bootstrap", "technology", "jquery", "node"];
+
+    axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then(response => {
+
+        arr.forEach(topic => {
+            response.data.articles[topic].forEach(item => {
+                const div = articleCard(item);
+            cardsContainer.append(div);
+            })
+        })
+    })
